8614d01d19dc2cda7b20f1f7fd02c90e28c547ad
Jose Antonio <esciper1@gmail.com>
2025-03-25 02:50:47 -0600

    install api and routes create

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(TaskController::class)->group(function () {
+    Route::get('/tasks', 'index');
+    Route::post('/task', 'store');
+    Route::get('/task/{id}', 'show');
+    Route::put('/task/{id}', 'update');
+    Route::patch('/task/{id}', 'updateStatus');
+    Route::delete('/task/{id}', 'destroy');
+});
